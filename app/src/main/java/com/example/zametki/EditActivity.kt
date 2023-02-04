89556a7ee693b0e5be38dc68b1f0f1c31a96eb16
bratanovv <bratanov.misha@mail.ru>
2023-02-04 04:26:00 +0300

    Add menue bar

--- app/src/main/java/com/example/zametki/EditActivity.kt
+++ app/src/main/java/com/example/zametki/EditActivity.kt
@@ -1,11 +1,54 @@
 package com.example.zametki
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.edit_activity.*
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.edit_activity)
+
+    }
+    var flStar = false
+    var flLock = false
+    fun onClickAddMenue(view: View) {
+        menueLayout.visibility = View.VISIBLE;
+        fbAddMenue.visibility = View.GONE
+        fbSave.visibility= View.GONE
+
+    }
+
+    fun onClickHideMenue(view: View) {
+
+        menueLayout.visibility=View.GONE;
+        fbAddMenue.visibility = View.VISIBLE
+        fbSave.visibility= View.VISIBLE
+
+    }
+
+
+    fun onClickStar(view: View) {
+        if (!flStar){
+            ibStar.setImageResource(R.drawable.ic_star)
+            flStar = true
+        }
+        else{
+            flStar=false
+            ibStar.setImageResource(R.drawable.ic_star_half)
+        }
+
+    }
+
+    fun onClickLock(view: View) {
+        if (!flLock){
+            flLock = true
+            ibLock.setImageResource(R.drawable.ic_lock_close)
+        }
+        else{
+            flLock = false
+            ibLock.setImageResource(R.drawable.ic_lock_open)
+        }
     }
 }
--- app/src/main/java/com/example/zametki/MainActivity.kt
+++ app/src/main/java/com/example/zametki/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.zametki
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.zametki.db.DbManager
 
@@ -16,33 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
     override fun onResume() {
         super.onResume()
         dbManager.openDb()
-//        val dataList = dbManager.readDbData()
-//        val tv= findViewById<TextView>(R.id.tvText)
-//        for(item in dataList){
-//
-//            tv.append(item)
-//            tv.append("\n")
-//        }
+
     }
     fun onClickNew(view: View) {
         val i = Intent(this,EditActivity::class.java)
         startActivity(i)
-//        val tv= findViewById<TextView>(R.id.tvText)
-//        tv.text=""
-//        val edt = findViewById<EditText>(R.id.edTitle)
-//        val edc = findViewById<EditText>(R.id.edContent)
-//        dbManager.insertToDb(edt.text.toString(),edc.text.toString())
-//        val dataList = dbManager.readDbData()
-//        for(item in dataList){
-//            tv.append(item)
-//            tv.append("\n")
-//        }
     }
 
     override fun onDestroy() {
@@ -50,3 +31,4 @@ class MainActivity : AppCompatActivity() {
         dbManager.closeDB()
     }
 }
+
